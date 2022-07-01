cd727de735235aa8dc8a2f4aa6830bfec0511fd0
tranvuadam <107505084+tranvuadam@users.noreply.github.com>
2022-07-01 13:44:01 +0200

    added blog and tag models

--- endevel_blog_app/blog/models.py
+++ endevel_blog_app/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from tag.models import Tag
+
+
+class BlogPost(models.Model):
+    title = models.CharField("Title", max_length=150)
+    detail = models.TextField("Detail", max_length=300)
+    text = models.TextField("Text", max_length=3000)
+    tags = models.ManyToManyField(Tag, related_name="blog_posts")
