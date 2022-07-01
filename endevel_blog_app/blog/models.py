a19c9755e79daf32bab3c8a1eb3584dce62bc0cb
tranvuadam <107505084+tranvuadam@users.noreply.github.com>
2022-07-01 14:34:19 +0200

    added image/file processing libs

--- endevel_blog_app/blog/models.py
+++ endevel_blog_app/blog/models.py
@@ -1,7 +1,17 @@
+import os
+
 from django.db import models
 
 # Create your models here.
 from tag.models import Tag
+
+
+def get_upload_path(instance, filename):
+    return os.path.join(instance.title, filename)
+
+
+def get_thumb_upload_path(instance, filename):
+    return os.path.join(instance.title, "thumbnail", filename)
 
 
 class BlogPost(models.Model):
@@ -9,3 +19,8 @@ class BlogPost(models.Model):
     detail = models.TextField("Detail", max_length=300)
     text = models.TextField("Text", max_length=3000)
     tags = models.ManyToManyField(Tag, related_name="blog_posts")
+    image = models.ImageField("Image", upload_to=get_upload_path, default="default.jpg")
+    thumbnail = models.ImageField("Thumbnail", upload_to=get_thumb_upload_path, default="default_thumb.jpg")
+
+    def filename(self):
+        return os.path.basename(self.image.name)
