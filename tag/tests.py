52da0ad65fd969553cd6702525b4c17600ace632
tranvuadam <107505084+tranvuadam@users.noreply.github.com>
2022-07-03 15:16:02 +0200

    added POST and PUT to tags + tests

--- tag/tests.py
+++ tag/tests.py
@@ -24,6 +24,20 @@ class TagTest(APITestCase):
         self.assertEqual(response.data[1], TagSerializer(Tag.objects.get(pk=2)).data)
         self.assertEqual(response.data[2], TagSerializer(Tag.objects.get(pk=1)).data)
 
+    def test_new_tag_post(self):
+        url = reverse('tag:tag_list')
+        data = {'name': "tag4"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data, TagSerializer(Tag.objects.get(pk=4)).data)
+
+    def test_new_tag_post_invalid_data(self):
+        url = reverse('tag:tag_list')
+        data = {'invalid_data': "tag4"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_tag_detail(self):
         url = reverse('tag:tag_detail', kwargs={'pk': 3})
         response = self.client.get(url, format='json')
@@ -36,3 +50,20 @@ class TagTest(APITestCase):
         url = reverse('tag:tag_detail', kwargs={'pk': 4})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_tag_detail_put(self):
+        url = reverse('tag:tag_detail', kwargs={'pk': 3})
+        new_name = "new_name"
+        data = {'name': new_name}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # num of fields
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data, TagSerializer(Tag.objects.get(pk=3)).data)
+        self.assertIn(new_name, TagSerializer(Tag.objects.get(pk=3)).data['name'])
+
+    def test_tag_detail_put_invalid_data(self):
+        url = reverse('tag:tag_detail', kwargs={'pk': 3})
+        data = {'invalid_data': "tag4"}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- tag/views.py
+++ tag/views.py
@@ -1,5 +1,6 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,13 +10,22 @@ from tag.serializers import TagSerializer
 
 
 class TagsList(APIView):
-    @swagger_auto_schema(responses={200: openapi.Response('Returns list of tags.', TagSerializer)},
-                         operation_id='Returns list of tags.',)
+    @swagger_auto_schema(responses={200: openapi.Response('Returns a list of Tags.', TagSerializer)},
+                         operation_id='Get a list of Tags.',)
     def get(self, request, format=None):
 
         tags = Tag.objects.all().order_by("-pk")
         serializer = TagSerializer(tags, many=True)
         return Response(serializer.data)
+
+    @swagger_auto_schema(responses={200: openapi.Response('Returns a created Blog post.', TagSerializer), 400: "Invalid data."},
+                         operation_id='Create a new Tag.', )
+    def post(self, request, format=None):
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagDetail(APIView):
@@ -23,8 +33,18 @@ class TagDetail(APIView):
         return get_object_or_404(Tag, pk=pk)
 
     @swagger_auto_schema(responses={200: openapi.Response('Returns details of a Tag.', TagSerializer), 404: "Tag not found."},
-                         operation_id='Returns details of a Tag.',)
+                         operation_id='Get details of a Tag.',)
     def get(self, request, pk, format=None):
         tag = self.get_object(pk)
         serializer = TagSerializer(tag)
         return Response(serializer.data)
+
+    @swagger_auto_schema(responses={200: openapi.Response('Returns details of an updated Tag.', TagSerializer), 404: "Tag not found.", 400: "Invalid data."},
+                         operation_id='Update a Tag.',)
+    def put(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
