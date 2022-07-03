d871e1b755b45f908a6561caa5e58bf647f02ca8
tranvuadam <107505084+tranvuadam@users.noreply.github.com>
2022-07-03 11:05:19 +0200

    added tag views and tests

--- tag/tests.py
+++ tag/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+from tag.models import Tag
+from tag.serializers import TagSerializer
+
+
+class TagTest(APITestCase):
+    def setUp(self):
+        Tag.objects.create(name="tag")
+        Tag.objects.create(name="tag2")
+        Tag.objects.create(name="tag3")
+
+    def test_tag_list(self):
+        url = reverse('tag:tag_list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(response.data[0], TagSerializer(Tag.objects.get(pk=3)).data)
+        self.assertEqual(response.data[1], TagSerializer(Tag.objects.get(pk=2)).data)
+        self.assertEqual(response.data[2], TagSerializer(Tag.objects.get(pk=1)).data)
+
+    def test_tag_detail(self):
+        url = reverse('tag:tag_detail', kwargs={'pk': 3})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # num of fields
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data, TagSerializer(Tag.objects.get(pk=3)).data)
+
+    def test_tag_detail_not_found(self):
+        url = reverse('tag:tag_detail', kwargs={'pk': 4})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- tag/views.py
+++ tag/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from tag.models import Tag
+from tag.serializers import TagSerializer
+
+
+class TagsList(APIView):
+    @swagger_auto_schema(responses={200: openapi.Response('Returns list of tags.', TagSerializer)},
+                         operation_id='Returns list of tags.',)
+    def get(self, request, format=None):
+
+        tags = Tag.objects.all().order_by("-pk")
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
+
+
+class TagDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Tag, pk=pk)
+
+    @swagger_auto_schema(responses={200: openapi.Response('Returns details of a Tag.', TagSerializer), 404: "Tag not found."},
+                         operation_id='Returns details of a Tag.',)
+    def get(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data)
